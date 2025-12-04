d44a777820ccafc741c346a44cb32dc78458cbb8
arafatshuvo990 <arafatshuvoworks@outlook.com>
2025-12-04 20:45:38 +0600

    Add controllers for stock

---
+++ Controllers/StockController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stock.Api.Data;
+
+namespace Stock.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult Index() {
+            var stocks = _context.Stock.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetStock(int id) {
+            var stock = _context.Stock.Find(id);
+            if (stock == null) {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stock.Api.Models;
+
+namespace Stock.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+        public DbSet<Stocks> Stock { get; set; }
+        public DbSet<Comments> Comments { get; set; }
+    }
+}
